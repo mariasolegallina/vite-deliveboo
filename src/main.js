4055f5d1c33bcf999d97e380588e05db934e5054
mariasolegallina <gallinamariasole@gmail.com>
2024-05-15 09:30:19 +0200

    add: bootstrap + sass

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
+import './style.scss'
 import App from './App.vue'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
 
 createApp(App).mount('#app')
