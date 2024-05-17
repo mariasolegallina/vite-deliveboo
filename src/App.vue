01c2e3aacca832657b775d2b4784f57ac71455e7
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-05-17 10:21:00 +0200

    feat-homepage layout

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,16 @@
 <script>
 
-import axios from 'axios';
-
 export default {
   data() {
     return {
-      restaurants: [],
-      types: [],
-
-      baseApiUrl: 'http://127.0.0.1:8000/api',
+      
     }
-  },
-
-  mounted() {
-    axios.get(this.baseApiUrl + '/restaurants').then(res => [
-      this.restaurants = res.data.results.data,
-      console.log(res.data)
-    ]),
-
-    axios.get(this.baseApiUrl + '/types').then(res=>[
-      this.types = res.data.results,
-      console.log(res.data.results)
-    ])
   },
 }
 </script>
 
 <template>
-<div class="container">
-  <h1>Ciao mondo</h1>
-  <button type="button" class="btn btn-primary">Primary</button>
 
-
-  <div>
-    <ul>
-      <li v-for="restaurant in restaurants">
-
-        {{ restaurant.restaurant_name }}
-
-
-      </li>
-    </ul>
-  </div>
-</div>
 </template>
 
 <style lang="scss">
