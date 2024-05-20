a9bcd6348ca1d19cab327ebc5e5816223fcd52b2
giosciafra <francescogioscia8@gmail.com>
2024-05-20 11:10:54 +0200

    feat: link user-fix

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,25 +1,39 @@
 <script>
 export default {
 
-name: 'PageHeader',
+    name: 'PageHeader',
+    data() {
+        return {
+            hover: false
+        };
+    },
+
+    methods: {
+        redirectToLogin() {
+            window.location.href = 'http://127.0.0.1:8000/';
+        }
+    }
 
 }
 </script>
 
 <template>
-<header>
-    <section>
-        <nav class="container">
-            <div class="logo">
-                <img src="/img/deliveboo-logo.png" alt="">
-            </div>
-            <div class="icons">
-                <i class="fa-solid fa-user"></i>
-                <i class="fa-solid fa-basket-shopping"></i>
-            </div>
-        </nav>
-    </section>
-</header>
+    <header>
+        <section>
+            <nav class="container">
+                <div class="logo">
+                    <img src="/img/deliveboo-logo.png" alt="">
+                </div>
+                <div class="icons">
+                    <div class="icon-container" @mouseover="hover = true" @mouseleave="hover = false">
+                        <i class="fa-solid fa-user" @click="redirectToLogin()"></i>
+                        <span v-show="hover" class="login-tooltip">Lavora con noi</span>
+                    </div>
+                    <i class="fa-solid fa-basket-shopping"></i>
+                </div>
+            </nav>
+        </section>
+    </header>
 </template>
 
 <style lang="scss" scoped>
@@ -30,7 +44,9 @@ header {
     background-color: $light;
     filter: drop-shadow(0 0 0.2rem $grey2);
 
-};
+}
+
+;
 
 nav {
     display: flex;
@@ -41,18 +57,45 @@ nav {
     .logo {
 
         max-width: 10rem;
-        
+
     }
 
     .icons {
         color: $grey8;
-        font-size: 1.4rem;   
+        font-size: 1.4rem;
 
         display: flex;
         align-items: center;
         gap: 1rem;
+
+        cursor: pointer;
+    }
+
+    .icon-container {
+        position: relative;
+        display: inline-block;
+    }
+
+    .login-tooltip {
+        font-size: 15px;
+        text-wrap: nowrap;
+        position: absolute;
+        top: 100%;
+        left: 50%;
+        transform: translateX(-50%);
+        background-color: #ef4860;
+        color: white;
+        padding: 4px 8px;
+        border-radius: 10px;
+        visibility: hidden;
+        opacity: 0;
+        transition: opacity 0.5s, visibility 0.5s;
+    }
+
+    .icon-container:hover .login-tooltip {
+        visibility: visible;
+        opacity: 1;
     }
 
 }
-
 </style>
