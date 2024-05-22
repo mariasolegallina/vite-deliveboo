4e1f175a7988656fd1b0dc588a90b012ff894957
mariasolegallina <gallinamariasole@gmail.com>
2024-05-23 01:51:28 +0200

    fix: basket layout

--- src/pages/Basket.vue
+++ src/pages/Basket.vue
@@ -80,88 +80,83 @@ export default {
 <section>
     <div class="container">
 
-        <h2 class="mb-4">Il tuo ordine</h2>
-
+        <!-- titolo e bottone home -->
+        <div class="page-top">
+            <router-link class="btn btn-outline-dark " :to="{name: 'home'}">
+                <i class="fa-solid fa-chevron-left"></i>
+            </router-link>
+            <h2 class="title">Il mio ordine</h2>
+        </div>
+        
         <!-- se il carrello contiene degli articoli li mostro -->
-        <div v-if="cart.length > 0" class="basket mb-5 p-3">
+        <div
+        v-if="cart.length > 0"
+        class="my-order"
+        >
 
-            <!-- dati ristorante -->
-            <div class="mb-4">
-                <h3>{{ cart[0].restaurantInfo.restaurant_name }}</h3>
-    
-                <p>{{ cart[0].restaurantInfo.address }}</p>
-            </div>
-
-            <div class="mb-5">
-
-                <!-- articoli  -->
-                <div v-for="item in cart" :key="item.dish.id" class="mb-4">
-                    <div class="d-flex gap-3 align-items-center justify-content-between">
-
-                        <!-- dati articoli -->
-                        <div class="mb-3 fw-semibold">
-                            {{ item.dish.name }} x {{ item.quantity }}
-                        </div>
-                        <div class="mb-3">
-                            € {{ dishSumPrice(item) }}
-                        </div>
-
-                    </div>
-
-                    <div class="mb-4">
-
-                        <!-- rimozione quantità articolo -->
-                        <button type="button" class="btn btn-outline-danger " @click="removeQuantity(item)">-</button>
-                        
-                        <span class="px-3">
-                            {{ item.quantity }}
-                        </span>
-
-                        <!-- aggiunta articolo -->
-                        <button type="button" class="btn btn-outline-danger " @click="addQuantity(item)">+</button>
-
-                        <!-- rimozione tutti gli articoli di quel tipo -->
-                        <button type="button" class="btn btn-outline-danger ms-3" @click="removeDish(item)">Rimuovi piatto</button>
-
-                    </div>
-
-                    <hr>
-                </div>
-
+            <div class="sidebar-r col-3">
+                <!-- totale carrello -->
                 <div class="text-end fs-4">
-
-                    <!-- totale carrello -->
                     <span>Totale:</span> <span class="fw-bold"> € {{ totalPrice }}</span>
                 </div>
 
-                
-            </div>
-            <!-- rimozione di tutti gli articoli dal carrello -->
-            <div class="d-flex justify-content-end">
-                <button type="button" class="btn btn-outline-danger ms-3" data-bs-toggle="modal" data-bs-target="#deleteModal">Svuota carrello</button>
-            </div>
+                <!-- rimozione di tutti gli articoli dal carrello -->
+                <button type="button" class="btn btn-success">Concludi l'ordine</button>
 
-            <!-- Modal per la rimozione -->
-            <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
+                <!-- rimozione di tutti gli articoli dal carrello -->
+                <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#deleteModal">Svuota il carrello</button>
 
-                        <div class="modal-body">
-                            Sei sicuro di voler svuotare il carrello? tutti i dati inseriti andranno persi
-                        </div>
+                <!-- Modal per la rimozione -->
+                <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
 
-                        <div class="modal-footer">
-                            <!-- annulla azione -->
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
-                            <!-- conferma azione -->
-                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal" @click="removeAll()">Svuota carrello</button>
+                            <div class="modal-body">
+                                Vuoi davvero svuotare il carrello? Tutti i dati inseriti andranno persi
+                            </div>
+
+                            <div class="modal-footer">
+                                <!-- annulla azione -->
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                                <!-- conferma azione -->
+                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal" @click="removeAll()">Prosegui</button>
+                            </div>
                         </div>
                     </div>
                 </div>
+
+                <!-- dati ristorante -->
+                <div class="restaurant">
+                    <h2 class="rest-name">{{ cart[0].restaurantInfo.restaurant_name }}</h2>
+                    <p>{{ cart[0].restaurantInfo.address }}</p>
+                    <p><i class="fa-solid fa-phone"></i> {{ cart[0].restaurantInfo.phone-number }}</p>
+                </div>
             </div>
 
+            <!-- dettagli ordine -->
+            <div
+            v-for="item in cart"
+            :key="item.dish.id"
+            class="order"
+            >                
+                <!-- dati articoli -->
+                <div class="order__dishes">
+                    <div class="dish-name">{{ item.dish.name }} x {{ item.quantity }}</div>
+                    <div class="dish-price">€ {{ dishSumPrice(item) }}</div>
+                </div>
 
-            
+                <div class="buttons">
+                    <!-- modifica quantità -->
+                    <div class="mod-quantity">
+                        <button type="button" class="btn-left" @click="removeQuantity(item)">-</button>                               
+                        <span class="number">{{ item.quantity }}</span>                               
+                        <button type="button" class="btn-right" @click="removeDish(item)">+</button>
+                    </div>
+
+                    <!-- rimozione tutti gli articoli di quel tipo -->
+                    <button type="button" class="btn btn-outline-danger btn-delete" @click="removeDish(item)"><i class="fa-solid fa-trash"></i></button>
+                </div> 
+            </div>                        
         </div>
 
         <!-- se nessun articolo risulta presente nel carrello -->
@@ -169,12 +164,6 @@ export default {
             Il tuo carrello è vuoto. <br>
             Torna indietro per vedere i nostri ristoranti.
         </div>
-
-
-        <!-- torna ai ristoranti -->
-        <router-link class="btn btn-outline-dark " :to="{ name: 'home' }">
-            <i class="fa-solid fa-chevron-left"></i> Indietro
-        </router-link>
 
     </div>
 </section>
@@ -185,22 +174,125 @@ export default {
 @use "../style/partials/variables" as *;
 @use "../style/partials/mixins" as *;
 
-.basket {
-    @include box1;
+.page-top {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 16px;
+    margin-bottom: 20px;
+
+    .title {
+        @include title-semi;
+    }
 
 }
 
-.basket h2 {
-    margin-bottom: 10px;
+.page-top .btn:hover i {
+        color: $light;
+    }
+
+.my-order {
+
+    display: flex;
+    flex-direction: row-reverse;
+    align-items: flex-start;
+    gap: 20px;
+
+    .sidebar-r {
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+    }
+
+    .restaurant {
+        @include box1;
+        padding: 10px 16px;
+        margin-bottom: 20px;
+        display: flex;
+        flex-direction: column;
+
+        background-color: $grey1;
+
+        .rest-name {
+            @include title2-semi;
+        }
+        p {
+            font-size: $txt5;
+            margin: 0;
+        }
+    }
+
+    .order {
+        @include box1;
+        padding: 20px;
+
+        display: flex;
+        flex: 1;
+
+        justify-content: space-between;
+        align-items: flex-start;
+
+        .dish-name {
+            @include title2-semi;
+        }
+
+        .buttons {
+            display: flex;
+            gap: 20px;
+        }
+
+    }
+
 }
 
-.basket ul {
-    list-style-type: none;
-    padding: 0;
-}
+.buttons {
+    .mod-quantity {
+    display: flex;
+    align-items: center;
+    }
 
-.basket li {
-    margin-bottom: 5px;
+    button {
+        background-color: white;
+        border: 1px solid $primary1;
+        padding: 10px;
+        cursor: pointer;
+        transition: background-color 0.3s;
+        outline: none;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        &:hover {
+            background-color: $primary1;
+            color: white;
+        }
+
+        &:first-of-type {
+            // border-right: none;
+            border-top-left-radius: 0.375rem;
+            border-bottom-left-radius: 0.375rem;
+        }
+
+        &:last-of-type {
+            // border-left: none;
+            border-top-right-radius: 0.375rem;
+            border-bottom-right-radius: 0.375rem;
+        }
+    }
+
+
+    .number {
+        padding: 10px;
+        border: 1px solid $primary1;
+        border-left: none;
+        border-right: none;
+        min-width: 40px;
+        text-align: center;
+    }
+
+    .btn-delete:hover i {
+        color: $light;
+    }
 }
 
 </style>
