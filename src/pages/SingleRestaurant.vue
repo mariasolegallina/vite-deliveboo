2cf3a803fd9700399ac028e47583efd02b648e24
mariasolegallina <gallinamariasole@gmail.com>
2024-05-22 22:54:44 +0200

    fix: SingleRestaurant layout

--- src/pages/SingleRestaurant.vue
+++ src/pages/SingleRestaurant.vue
@@ -96,35 +96,39 @@ export default {
         <div class="container">
             <h3 class='rest_title'>{{ restaurant.restaurant_name }}</h3>
             <p>{{ restaurant.address }}</p>
+
+            <!-- wrong restaurant error message -->
             <div v-if="errorMessage" class="alert alert-danger">{{ errorMessage }}</div>
-            <ul class="list-group mb-5">
-                <li 
+
+            <!-- dishes list -->
+            <ul>
+                <li
                     v-for="dish in restaurant.dishes"
                     v-show="dish.viewable"
                     class="list-group-item"
                 >
                     <div class="dish-card">
-                        <div class="dish-card__left">
+                        <div class="dish-card_img">
                             <img :src="'http://localhost:8000/storage/' + dish.image " alt="">
-                            <div class="texts">
+                        </div>
+                        <div class="dish-card_text">
+                            <div class="dish-info">
                                 <h3>{{ dish.name }}</h3>
                                 <p>{{ dish.description }}</p>
                                 <span>€ {{ dish.price }}</span>
                             </div>
+
+                            <!-- add to cart -->
+                            <div class="add-to-cart">
+                                <button type="button" class="btn-left" @click="removeFromCart(dish, 1)">-</button>                               
+                                <span class="number">{{ getQuantity(dish) }}</span>                               
+                                <button type="button" class="btn-right" @click="addToCart(dish, 1)">+</button>
+                            </div>
                         </div>
-                        
-                        
-                        <div class="dish-card__right">
-                            <button type="button" class="btn btn-outline-danger " @click="removeFromCart(dish, 1)">-</button>
-                            
-                            <span class="px-3">{{ getQuantity(dish) }}</span>
-                            
-                            <button type="button" class="btn btn-outline-danger " @click="addToCart(dish, 1)">+</button>
-                        </div>
-                    </div>                
+                    </div>   
                 </li>
             </ul>
-            
+
             <router-link class="btn btn-outline-dark " :to="{name: 'home'}">
                 <i class="fa-solid fa-chevron-left"></i> Indietro
             </router-link>
@@ -141,23 +145,99 @@ export default {
 margin-bottom: 20px;
 }
 
-.dish-card {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+ul {
+    padding: 0;
 
-    .dish-card__left {
-        display: flex;
-        align-items: center;
-        max-width: 70%;
-    }
-
-    .texts {
-        margin-left: 15px;
+    li {
+        margin-bottom: 20px
     }
 }
-.dish-card__rigth {
-    max-width: 30%;
+
+.dish-card {
+    @include box1;
+
+    display: flex;
+    align-items: stretch;
+    width: 100%;
+
+    .dish-card_img {
+        width: 40%;
+
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+    }
+
+    .dish-card_text {
+        flex: 1;
+        padding: 20px;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+
+        .dish-info {
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            margin-bottom: 14px;
+
+            p {
+                margin: 0 0 6px 0;
+                padding: 0;
+
+                .title {
+                    font-weight: 600;
+                }
+            }
+        }
+
+        .add-to-cart {
+            display: flex;
+            align-items: center;
+        }
+
+        button {
+        background-color: white;
+        border: 1px solid $primary1;
+        padding: 10px;
+        cursor: pointer;
+        transition: background-color 0.3s;
+        outline: none;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        &:hover {
+            background-color: $primary1;
+            color: white;
+        }
+
+        &:first-of-type {
+            // border-right: none;
+            border-top-left-radius: 0.375rem;
+            border-bottom-left-radius: 0.375rem;
+        }
+
+        &:last-of-type {
+            // border-left: none;
+            border-top-right-radius: 0.375rem;
+            border-bottom-right-radius: 0.375rem;
+        }
+        }
+
+
+        .number {
+        padding: 10px;
+        border: 1px solid $primary1;
+        border-left: none;
+        border-right: none;
+        min-width: 40px; /* Assicura che l'elemento non si deformi */
+        text-align: center;
+        }
+    }
 }
 
 </style>
