5b77286f0224576653e3c58756034ab07354ea9d
mariasolegallina <gallinamariasole@gmail.com>
2024-05-28 01:48:29 +0200

    add: style

--- src/pages/OrderSuccess.vue
+++ src/pages/OrderSuccess.vue
@@ -13,10 +13,26 @@ export default {
 </script>
 
 <template>
-    <div class="container">
-        Ordine effettuato con successo!
-    </div>
+    <section>
+        <div class="container">
+
+            <h2>
+                Ordine effettuato con successo!
+            </h2>
+            <p>
+                Controlla la tua casella di posta per avere tutti i dettagli.
+            </p>
+
+            <router-link class="btn btn-outline-dark " :to="{ name: 'home' }">
+                    <i class="fa-solid fa-chevron-left"></i> Torna alla home
+            </router-link>
+        </div>
+    </section>
 </template>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
+@use "../style/partials/variables" as *;
+@use "../style/partials/mixins" as *;
+
+
 </style>
