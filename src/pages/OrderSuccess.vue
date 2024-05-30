1c65978b34ddcd9dad9efbd217aaa99035b3dfa3
mariasolegallina <gallinamariasole@gmail.com>
2024-05-30 16:57:24 +0200

    layout

--- src/pages/OrderSuccess.vue
+++ src/pages/OrderSuccess.vue
@@ -75,29 +75,41 @@ export default {
     <section>
         <div class="container">
 
-            <h2>
-                Ordine effettuato con successo!
-            </h2>
-            <p>
-                Controlla la tua casella di posta per avere tutti i dettagli.
-                Intanto ecco un riepilogo!
-            </p>
+            <div class="order-success">
+                <h2>
+                    Ordine effettuato con successo!
+                </h2>
+                <p>
+                    Controlla la tua casella di posta per avere tutti i dettagli.
+                </p>   
+            </div>
+            
+            <div class="details">
+                <!-- order details -->
+                <div class="order-details">
+                    <h3>Riepilogo ordine</h3>
+                    <p v-for="item in tempCart" >
+                        {{ item.dish.name }} x {{ item.quantity }} = € {{ dishSumPrice(item) }}
+                    </p>
+                    <hr>
+                    <p class="fw-semibold">
+                        Totale: € {{ total_price }}
+                    </p>
+                </div>
 
-            <h3>Dati ristorante</h3>
-            <p>
-                <div>{{ tempCart[0].restaurantInfo.restaurant_name }}</div>
-                <div>{{ tempCart[0].restaurantInfo.address }}</div>
-                <div>+39 {{ tempCart[0].userInfo.phone_number }}</div>
-            </p>
-
-            <h3>Riepilogo ordine</h3>
-            <p v-for="item in tempCart" >
-                {{ item.dish.name }} x {{ item.quantity }} = € {{ dishSumPrice(item) }}
-            </p>
-            <p>
-                Totale: € {{ total_price }}
-            </p>
-
+                <!-- restaurant info -->
+                <div class="rest-info col-md-3">
+                    <h2 class="title">{{ tempCart[0].restaurantInfo.restaurant_name }}</h2>
+                    <p>
+                        <span>
+                            <i class="fa-solid me-2 fa-location-dot"></i>{{ tempCart[0].restaurantInfo.address }}  
+                        </span>
+                        <span>
+                            <i class="fa-solid me-2 fa-phone"></i>+39 {{ tempCart[0].userInfo.phone_number }}  
+                        </span>
+                    </p>
+                </div>
+            </div>
 
             <router-link class="btn btn-outline-dark " :to="{ name: 'home' }">
                 <i class="fa-solid fa-chevron-left"></i> Torna alla home
@@ -109,4 +121,101 @@ export default {
 <style lang="scss" scoped>
 @use "../style/partials/variables" as *;
 @use "../style/partials/mixins" as *;
+
+.container {
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+    align-items: flex-start;
+
+    .order-success {
+        display: flex;
+        flex-direction: column;
+        gap: 6px;
+
+        h2 {
+            @include title2-semi;
+
+            margin: 0;
+            padding: 0;
+        }
+
+        p {
+            @include title3;
+            margin: 0;
+            padding: 0;
+        }
+    }
+
+}
+
+.details {
+
+    display: flex;
+    gap: 24px;
+    
+    @media (max-width: 992px) {
+        flex-direction: column;
+    }
+
+    // order details
+    .order-details {
+        @include box1;
+        padding: 24px;
+        flex-grow: 1;
+
+        display: flex;
+        flex-direction: column;
+        gap: 6px;
+
+        h2 {
+            @include title2-semi;
+
+            margin: 0 0 4px;
+            padding: 0;
+        }
+
+        p {
+            margin: 0;
+            padding: 0;
+            @include title3;
+        }
+
+    }
+
+    // restaurant info
+    .rest-info {
+        @include box1;
+        padding: 24px;
+
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        background-color: $grey1;
+
+        .title {
+            @include title3-semi;
+            margin: 0;
+            padding: 0;
+
+        }
+
+        p {
+            font-size: $txt5;
+            margin: 0;
+
+            display: flex;
+            flex-direction: column;
+            gap: 8px;
+
+            i {
+                margin: 0 2px 0px;
+
+                &:first-of-type {
+                    margin-left: 0;
+                }
+            }
+        }
+    }
+}
 </style>
